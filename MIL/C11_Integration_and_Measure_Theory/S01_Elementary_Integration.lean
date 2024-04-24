56f655caf9ed97f66957a93b8b9fef249753a52e
Xiaoxin Yin <xiaoxin@gmail.com>
2024-04-24 11:43:08 -0700

    Restore more

--- MIL/C11_Integration_and_Measure_Theory/S01_Elementary_Integration.lean
+++ MIL/C11_Integration_and_Measure_Theory/S01_Elementary_Integration.lean
@@ -16,21 +16,21 @@ open MeasureTheory intervalIntegral
 open Interval
 -- this introduces the notation `[[a, b]]` for the segment from `min a b` to `max a b`
 
-theorem ex1 (a b : ℝ) : (∫ x in a..b, x) = (b ^ 2 - a ^ 2) / 2 :=
+example (a b : ℝ) : (∫ x in a..b, x) = (b ^ 2 - a ^ 2) / 2 :=
   integral_id
 
-theorem ex2 {a b : ℝ} (h : (0 : ℝ) ∉ [[a, b]]) : (∫ x in a..b, 1 / x) = Real.log (b / a) :=
+example {a b : ℝ} (h : (0 : ℝ) ∉ [[a, b]]) : (∫ x in a..b, 1 / x) = Real.log (b / a) :=
   integral_one_div h
 
-theorem ex3 (f : ℝ → ℝ) (hf : Continuous f) (a b : ℝ) : deriv (fun u ↦ ∫ x : ℝ in a..u, f x) b = f b :=
+example (f : ℝ → ℝ) (hf : Continuous f) (a b : ℝ) : deriv (fun u ↦ ∫ x : ℝ in a..u, f x) b = f b :=
   (integral_hasStrictDerivAt_right (hf.intervalIntegrable _ _) (hf.stronglyMeasurableAtFilter _ _)
         hf.continuousAt).hasDerivAt.deriv
 
-theorem ex4 {f : ℝ → ℝ} {a b : ℝ} {f' : ℝ → ℝ} (h : ∀ x ∈ [[a, b]], HasDerivAt f (f' x) x)
+example {f : ℝ → ℝ} {a b : ℝ} {f' : ℝ → ℝ} (h : ∀ x ∈ [[a, b]], HasDerivAt f (f' x) x)
     (h' : IntervalIntegrable f' volume a b) : (∫ y in a..b, f' y) = f b - f a :=
   integral_eq_sub_of_hasDerivAt h h'
 
 open Convolution
 
-theorem ex5 (f : ℝ → ℝ) (g : ℝ → ℝ) : f ⋆ g = fun x ↦ ∫ t, f t * g (x - t) :=
+example (f : ℝ → ℝ) (g : ℝ → ℝ) : f ⋆ g = fun x ↦ ∫ t, f t * g (x - t) :=
   rfl
--- MIL/C11_Integration_and_Measure_Theory/S02_Measure_Theory.lean
+++ MIL/C11_Integration_and_Measure_Theory/S02_Measure_Theory.lean
@@ -11,39 +11,39 @@ noncomputable section
 
 variable {α : Type*} [MeasurableSpace α]
 
-theorem ex1 : MeasurableSet (∅ : Set α) :=
+example : MeasurableSet (∅ : Set α) :=
   MeasurableSet.empty
 
-theorem ex2 : MeasurableSet (univ : Set α) :=
+example : MeasurableSet (univ : Set α) :=
   MeasurableSet.univ
 
-theorem ex3 {s : Set α} (hs : MeasurableSet s) : MeasurableSet (sᶜ) :=
+example {s : Set α} (hs : MeasurableSet s) : MeasurableSet (sᶜ) :=
   hs.compl
 
-theorem ex4 : Encodable ℕ := by infer_instance
+example : Encodable ℕ := by infer_instance
 
-theorem ex5 (n : ℕ) : Encodable (Fin n) := by infer_instance
+example (n : ℕ) : Encodable (Fin n) := by infer_instance
 
 variable {ι : Type*} [Encodable ι]
 
-theorem ex6 {f : ι → Set α} (h : ∀ b, MeasurableSet (f b)) : MeasurableSet (⋃ b, f b) :=
+example {f : ι → Set α} (h : ∀ b, MeasurableSet (f b)) : MeasurableSet (⋃ b, f b) :=
   MeasurableSet.iUnion h
 
-theorem ex7 {f : ι → Set α} (h : ∀ b, MeasurableSet (f b)) : MeasurableSet (⋂ b, f b) :=
+example {f : ι → Set α} (h : ∀ b, MeasurableSet (f b)) : MeasurableSet (⋂ b, f b) :=
   MeasurableSet.iInter h
 
 open MeasureTheory
 variable {μ : Measure α}
 
-theorem ex8 (s : Set α) : μ s = ⨅ (t : Set α) (_ : s ⊆ t) (_ : MeasurableSet t), μ t :=
+example (s : Set α) : μ s = ⨅ (t : Set α) (_ : s ⊆ t) (_ : MeasurableSet t), μ t :=
   measure_eq_iInf s
 
-theorem ex9 (s : ι → Set α) : μ (⋃ i, s i) ≤ ∑' i, μ (s i) :=
+example (s : ι → Set α) : μ (⋃ i, s i) ≤ ∑' i, μ (s i) :=
   measure_iUnion_le s
 
-theorem ex10 {f : ℕ → Set α} (hmeas : ∀ i, MeasurableSet (f i)) (hdis : Pairwise (Disjoint on f)) :
+example {f : ℕ → Set α} (hmeas : ∀ i, MeasurableSet (f i)) (hdis : Pairwise (Disjoint on f)) :
     μ (⋃ i, f i) = ∑' i, μ (f i) :=
   μ.m_iUnion hmeas hdis
 
-theorem ex11 {P : α → Prop} : (∀ᵐ x ∂μ, P x) ↔ ∀ᶠ x in μ.ae, P x :=
+example {P : α → Prop} : (∀ᵐ x ∂μ, P x) ↔ ∀ᶠ x in μ.ae, P x :=
   Iff.rfl
